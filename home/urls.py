fc239487d36efae7e5047716b8036cbf174d02f8
Anurag94507 <mranurag0007@gmail.com>
2025-09-25 00:03:02 +0530

    50720

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import MenuCategoryListAPIView
+
+app_name = 'home'
 
 urlpatterns = [
-    
+    path('api/categories', MenuCategoryListAPIView(), name='api-menu-categories'),
 ]
