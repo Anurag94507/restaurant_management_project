9eb608a2e8be9ce757a4823d172f89bcd09ec9b3
Anurag94507 <mranurag0007@gmail.com>
2025-09-24 23:59:29 +0530

    50720

--- home/views.py
+++ home/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import Menucategory
+from .serializers import MenucategorySerializers
+
+class MenucategoryListAPIView(generics.ListAPIView):
+    """
+    API endpoint to list all menu categories.
+    Only Get is allowed (read-only list).
+    """
+    queryset = Menucategory.objects.all()
+    serializer_class = MenucategorySerializer
