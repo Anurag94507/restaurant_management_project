798f4c62862dd81b5aa51b5f83055870601a0fd0
Anurag94507 <mranurag0007@gmail.com>
2025-09-25 00:06:04 +0530

    50720

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import path, include
 
 urlpatterns = [
-    
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+
 ]
