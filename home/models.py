d9e8c7a3af86bd7df6474525b5d0b0a5812bd1f1
Anurag94507 <mranurag0007@gmail.com>
2025-09-27 15:38:24 +0530

    51164

--- home/models.py
+++ home/models.py
@@ -5,3 +5,12 @@ class MenuCategory(models.Model):
 
     def __str__(self):
         return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.Foreignkey(MenuCategory, on_delete=models.SET_NULL, null=True, related_name='items')
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
